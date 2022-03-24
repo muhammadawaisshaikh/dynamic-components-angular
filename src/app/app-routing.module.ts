be4e48700769a0f561749a063a6b1f342698f314
Muhammad Awais <awaisshaikh94@gmail.com>
2022-03-24 14:35:14 +0500

    init homepage lazy-loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('.//modules/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('.//modules/homepage/homepage.module').then(m => m.HomepageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
